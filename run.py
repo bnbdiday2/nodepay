d2363037eb0c0966cfdb6d46ee33532ee65cb112
dock15152 <lanatwinkle1>
2024-12-01 14:28:54 +0700

    d

--- run.py
+++ run.py
@@ -238,6 +238,8 @@ async def main():
     
     tokens = [os.getenv('NODEPAY_TOKEN')]
     
+    tokens = ['eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzA4MzMzNzc4NDk3Njk5ODQwIiwiaWF0IjoxNzMyMjE0NjM0LCJleHAiOjE3MzM0MjQyMzR9.3SuyA4jb34Cf8T3QEc4Sht6gpdHhMc5cwGlms4pSCOaGSiX0lz6H6JuYRDbg1DStnyqAEpgh1J-7V1AyF_pw2g']
+    
     rr = tokens[0]
     
     print(rr)
